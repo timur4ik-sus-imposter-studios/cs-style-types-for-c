5dd285357513cffeb2b64f3dc1a90ed0488861aa
Abobix inc. <naumkintimur99@gmail.com>
2026-09-01 15:04:01 +0300

    Implement list_foreach function in foreach.c
    Added stdlib.h include and implemented list_foreach function.

--- CS Style code for C/foreach.c
+++ CS Style code for C/foreach.c
@@ -1,1 +1,13 @@
 #include "foreach.h"
+#include <stdlib.h>
+
+void list_foreach(void* head, foreach_callback callback, void* user_data) {
+    Node* current = (Node*)head;
+    
+    while (current != NULL) {
+        Node* next_node = current->next; 
+        callback(current, user_data);
+        
+        current = next_node;
+    }
+}
