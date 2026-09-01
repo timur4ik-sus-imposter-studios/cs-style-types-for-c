41ef62131ed401b05e5db03370a0c6af6d2fadf3
Abobix inc. <naumkintimur99@gmail.com>
2026-09-01 15:05:53 +0300

    Refactor list_T.h to use generic List structure

--- CS Style code for C/list_T.h
+++ CS Style code for C/list_T.h
@@ -1,33 +1,18 @@
 #ifndef LIST_H
 #define LIST_H
-#include <stdlib.h> 
-#define DEFINE_LIST(T) \
-typedef struct { \
-    T* data; \
-    size_t count; \
-    size_t size; \
-} List_##T; \
-\
-void List_##T##_new(list_##T* v) { \
-    v->data = NULL; \
-    v->count= v->size = 0; \
-} \
-\
-void Llist_##T##_push(list_##T* v, T elem) { \
-    if (v->count == v->size) { \
-        v->size = v->size ? v->size << 1 : 1; \
-        v->data = (T*)realloc(v->data, v->size * sizeof(T)); \
-    } \
-    v->data[v->count++] = elem; \
-} \
-\
-T List_##T##_pop(list_##T* v) { \
-    return v->data[--v->count]; \
-} \
-\
-void List_##T##_free(list_##T* v) { \
-    free(v->data); \
-    list_##T##_new(v); \
-}
 
-#endif // MY_DLL_H
+#include <stdlib.h> 
+
+typedef struct {
+    void* data;          
+    size_t element_size;
+    size_t count;        
+    size_t size;      
+} List;
+
+void list_new(List* v, size_t element_size);
+void list_push(List* v, const void* elem);
+void list_pop(List* v, void* out_elem);
+void list_free(List* v);
+
+#endif // LIST_H
